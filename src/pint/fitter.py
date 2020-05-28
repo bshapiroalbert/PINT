9118ce841979f7c88d0ab25df9249177ad2ff5aa
Jing Luo <luojing1211@gmail.com>
2020-05-28 16:54:44 -0400

    Update the code

--- src/pint/fitter.py
+++ src/pint/fitter.py
@@ -176,7 +176,7 @@ class Fitter(object):
 
     def get_summary(self, nodmx=False):
         """Return a human-readable summary of the Fitter results.
-        
+
         Parameters
         ----------
         nodmx : bool
@@ -892,3 +892,20 @@ class GLSFitter(Fitter):
                 self.resids.noise_resids = noise_resids
 
         return chi2
+
+
+class GlobalFitter(): # Is GLSFitter the best here?
+    """ GlobalFitter aims to fit mulitply data set together.
+
+    Parameter
+    ---------
+    data_model: list
+        The input data model object pair list. The format is [(data, model)]
+    residual_type: one residual class or a list of residual class, optional.
+        If data-model pairs uses more than one type of residuals, the full list
+        of residual class for each data-model pair are required. Default is the
+        TOA residuals.
+    """
+
+    def __init__(self, data_model, residual_type=Residuals):
+        self.method = "global_least_square"
